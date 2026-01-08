18722181f283facc603f697442d299dc70c0352a
Abhijit Pathare <abhijitpathare2006@gmail.com>
2026-01-08 14:44:48 +0530

    Change MySQL connection settings
    Updated MySQL connection parameters with new credentials.

--- mysql_connection.js
+++ mysql_connection.js
@@ -3,10 +3,10 @@ var util = require("util");
 require("dotenv").config();
 
 var connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host:bbulbsztxm12brevpywu-mysql.services.clever-cloud.com,
+    user: uspvook8ktdjmybrprocess.env.USER,
+    password: vNheCOlfQSao7ktGUc0K,
+    database: bbulbsztxm12brevpywu,
 });
 
 var exe = util.promisify(connection.query).bind(connection);

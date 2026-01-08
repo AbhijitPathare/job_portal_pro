ba381427ce9023ac5e31d3d0020905fa580744b5
Abhijit Pathare <abhijitpathare2006@gmail.com>
2026-01-08 16:06:23 +0530

    Use environment variables for MySQL connection

--- mysql_connection.js
+++ mysql_connection.js
@@ -3,10 +3,10 @@ var util = require("util");
 require("dotenv").config();
 
 var connection = mysql.createConnection({
-    host:bbulbsztxm12brevpywu-mysql.services.clever-cloud.com,
-    user: uspvook8ktdjmybrprocess.env.USER,
-    password: vNheCOlfQSao7ktGUc0K,
-    database: bbulbsztxm12brevpywu,
+    host:process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 var exe = util.promisify(connection.query).bind(connection);
